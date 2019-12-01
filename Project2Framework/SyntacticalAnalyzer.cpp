1e27ee1589a2322710458507fd965949b9f0802a
Jacob Blechel <blek@Jacobs-iMac.local>
2019-11-30 18:18:59 -0800

    contructor now opens files

--- Project2Framework/SyntacticalAnalyzer.cpp
+++ Project2Framework/SyntacticalAnalyzer.cpp
@@ -9,12 +9,42 @@
 #include <iostream>
 #include <iomanip>
 #include <fstream>
+#include <string>
 #include "SyntacticalAnalyzer.h"
 
 using namespace std;
 
 SyntacticalAnalyzer::SyntacticalAnalyzer (char * filename)
 {
+	string DBGfilename(filename);
+	DBGfilename.erase(DBGfilename.size()-3);//removes .ss extension
+	string LSTfilename = DBGfilename;
+	string P2filename  = DBGfilename;
+	DBGfilename += "2.dbg";
+	LSTfilename += "2.lst";
+	P2filename  += ".P2";
+
+	dbg.open(DBGfilename,std::ofstream::out); //first open .dbg file
+	if(dbg.is_open()){
+
+		lst.open(LSTfilename,std::ofstream::out); //and on..
+		if(lst.is_open()){
+			write_to_lst("Input file: " + string(filename) + "\n");
+			p2.open(P2filename,std::ofstream::out);
+			if(p2.is_open()){
+				//all files open.
+			} else{
+				report_error("Error Opening " + string(P2filename) + "\n");
+			}
+		} else{
+			report_error("Error opening " + string(LSTfilename) + "\n");
+		}
+	} else{
+		cout << "Error opening " + string(DBGfilename) + "\n";
+	}
+
+
+
 	lex = new LexicalAnalyzer (filename);
 	token_type t;
 	t = lex->GetToken();
@@ -31,6 +61,19 @@ SyntacticalAnalyzer::SyntacticalAnalyzer (char * filename)
 	}
 
 }
+
+void SyntacticalAnalyzer::write_to_lst(const string & msg){
+
+}
+
+void SyntacticalAnalyzer::write_to_p2(const string & msg) {
+
+}
+void SyntacticalAnalyzer::report_error(const string & msg){
+
+}
+
+
 
 SyntacticalAnalyzer::~SyntacticalAnalyzer ()
 {
